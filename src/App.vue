e78d08b7d52837b59ba32d51c2bde18a781d832d
MuJin <macsouz6104@gmail.com>
2022-07-26 23:48:59 +0800

    基礎結構

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import RwvHeader from "./components/navbar.vue";
 </script>
 
 <template>
+  <RwvHeader />
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
---
+++ src/components/navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+      <a class="navbar-brand ms-4" href="./index.html">Side project demo</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-2">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="./test.html">test page</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#pic">地球概論</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#intro">生態系統</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#inform">人類的影響</a>
+            </li>
+            <li class="nav-item me-4">
+              <a class="nav-link" href="#sec">現代</a>
+            </li>
+            <div class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</template>
